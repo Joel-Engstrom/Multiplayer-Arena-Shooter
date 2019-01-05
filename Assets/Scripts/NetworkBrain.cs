52b185391116ea21187c8585ad46c272806d5b9e
Joel <mang.hansson@gmail.com>
2019-01-05 21:15:51 +0100

    Meny update

---
+++ Assets/Scripts/NetworkBrain.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkBrain : NetworkManager
+{
+    public override void OnStartServer()
+    {
+        base.OnStartServer();
+    }
+
+    public override void OnStopServer()
+    {
+        base.OnStopServer();
+    }
+
+}
