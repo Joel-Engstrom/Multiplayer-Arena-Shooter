56ce59dccdf561bc05b7ac8bfc75400254575fb3
Joel <mang.hansson@gmail.com>
2019-01-08 00:40:46 +0100

    Uppdaterade Player prefab samt Lobbyn

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using InControl;
+using Cinemachine;
+
+public class CameraController : MonoBehaviour
+{
+    public CinemachineFreeLook Cam;
+
+    private InputDevice Controller = InputManager.ActiveDevice;
+
+    private float CameraYValue;
+    private float CameraXValue;
+
+    private void Update()
+    {
+        //Uppdatera vilken kontroller som används.
+        Controller = InputManager.ActiveDevice;
+        //Spara Värdena på X och Y till kameran.
+        CameraXValue = Controller.RightStickX.Value;
+        CameraYValue = Controller.RightStickY.Value;
+        //Uppdatera kamerans position.
+        Cam.m_XAxis.m_InputAxisValue = CameraXValue;
+        Cam.m_YAxis.m_InputAxisValue = CameraYValue;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using InControl;
+
+public class PlayerController : MonoBehaviour
+{
+    InputDevice ActiveController;
+
+    public float Speed;
+
+    public LayerMask GroundLayer;
+    public GameObject GroundCheck;
+
+    private CharacterController Controller;
+    private Camera PlayerCam;
+    private float ValueX;
+    private float ValueZ;
+    private bool isGrounded;
+    private Vector3 DesiredMoveDirection;
+    private bool BlockRotationPlayer;
+    private bool BlockRotationSpeed;
+    public float DesiredRotationSpeed;
+    public float AllowPlayerRotation;
+    private Vector3 MoveVector;
+    private float VerticalVel;
+
+
+    private void Awake()
+    {
+        Controller = GetComponent<CharacterController>();
+        PlayerCam = GetComponentInChildren<Camera>();
+    }
+
+    private void Update()
+    {
+        InputMagnitude();
+
+        isGrounded = Controller.isGrounded;
+
+        ActiveController = InputManager.ActiveDevice;
+
+        ValueX = ActiveController.GetControl(InputControlType.LeftStickX);
+        ValueZ = ActiveController.GetControl(InputControlType.LeftStickY);
+
+        if (isGrounded)
+        {
+            VerticalVel -= 0;
+        } else
+        {
+            VerticalVel -= 2;
+        }
+        MoveVector = new Vector3(0, VerticalVel, 0);
+        Controller.Move(MoveVector);
+
+    }
+
+    private void PlayerAndMoveRotation()
+    {
+        var forward = PlayerCam.transform.forward;
+        var right = PlayerCam.transform.right;
+
+        forward.y = 0f;
+        right.y = 0f;
+
+        forward.Normalize();
+        right.Normalize();
+
+        DesiredMoveDirection = forward * ValueZ + right * ValueX;
+
+        if (BlockRotationPlayer == false)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(DesiredMoveDirection), DesiredRotationSpeed);
+        }
+    }
+
+    private void InputMagnitude()
+    {
+        Speed = new Vector2(ValueX, ValueZ).sqrMagnitude;
+
+        if (Speed > AllowPlayerRotation)
+        {
+            PlayerAndMoveRotation();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (Physics.CheckSphere(GroundCheck.transform.position, .3f, GroundLayer))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+
+
+
+        
+    }
+}
