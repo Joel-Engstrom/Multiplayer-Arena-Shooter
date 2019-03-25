b9c41edd0f55cc2add6b44ac2ecebab04e5c056d
edvin hedin <edvin.hedin@elev.solleftea.se>
2019-03-25 13:09:26 +0100

    Lite färger, och trappan

---
+++ Assets/Scripts/DeathChecker.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathChecker : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.gameObject.transform.position = new Vector3(-5, 21, 7);
+            Debug.Log("entered");
+        }
+    }
+}
