e03dd85cb5847b4dd241f5f37f284b1c00d94e94
Edvin Hedin <46488238+edvin-hedin@users.noreply.github.com>
2019-04-12 18:36:19 +0200

    La till ett reloading system + en lätt animation, tog bort onödiga prefabs

--- Assets/Scripts/PistolScript.cs
+++ Assets/Scripts/PistolScript.cs
@@ -7,17 +7,47 @@ public class PistolScript : MonoBehaviour
     public float damage = 10f;
     public float range = 100f;
     public Camera fpsCam;
-    
+    public int maxAmmo = 10;
+    private int currentAmmo;
+    public float reloadTime = 1f;
+    private bool isReloading = false;
+    public Animator animator;
+
+
+    void Start()
+    {
+        currentAmmo = maxAmmo;
+    }
     void Update()
     {
+        if (isReloading)
+        {
+            return;
+        }
+        if (currentAmmo <= 0)
+        {
+            StartCoroutine(Reload());
+            return;
+        }
         if (Input.GetButtonDown("Fire1"))
         {
             Shoot();
         }
-       
+    }
+    IEnumerator Reload()
+    {
+        isReloading = true;
+        Debug.Log("Reloading...");
+        animator.SetBool("isReloading", true);
+        yield return new WaitForSeconds(reloadTime -.25f);
+        animator.SetBool("isReloading", false);
+        yield return new WaitForSeconds(.25f);
+        currentAmmo = maxAmmo;
+        isReloading = false;
     }
     void Shoot()
     {
+        currentAmmo--;
         RaycastHit hit;
         if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
         {
